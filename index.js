7db5b1986c652c38aeee2513cbdc2183e7c8ea7a
Anshuljkt <anshul.jkt@gmail.com>
2019-10-08 17:28:23 -0400

    Started work on Joke API

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Import stylesheets
 import './style.css';
+
+const joke = document.getElementById("joke");
+const button = document.getElementById("jokeButton");
+
+const url = "https://icanhazdadjoke.com/"
+
+document.addEventListener ('click', (event) => {
+  if (event.target === button) {
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function(data) {
+        console.log(resp);
+        console.log(data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+    // joke.innerHTML = "";
+  }
+})
