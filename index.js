d76c185deb58c00f37c0ac0b752de90a7cb2e877
Anshuljkt <anshul.jkt@gmail.com>
2019-10-08 21:17:03 -0400

    1 binaries uploaded

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 const joke = document.getElementById("joke");
 const button = document.getElementById("jokeButton");
+const gif = document.getElementById("gif");
 
 const url = 'https://icanhazdadjoke.com/';
 
@@ -12,6 +13,7 @@ const options = {
     "Accept": 'application/json'
   } 
 }
+
 
 function newJoke() {
    fetch(url, options)
@@ -25,14 +27,32 @@ function newJoke() {
     .catch(error => {
       console.err(error);
     })
-  
+}
+
+const gifLink = "https://api.giphy.com/v1/gifs/search?api_key=hocrXROrKzZNjR9TKOdwfEt1OsEcK2Hr&q=cringe&limit=1&offset=0&rating=PG-13&lang=en"
+
+function gifSearch() {
+   fetch(gifLink)
+    .then(resp1 => {
+      return resp1.json();
+    })
+    .then(json1 => {
+      newURL = json1.data[0].url;
+      console.log(json1);
+      console.log(newURL);
+      gif.src = newURL;
+    })
+    .catch(error1 => {
+      console.err(error1);
+    })
 }
 
 newJoke();
 
 document.addEventListener ('click', (event) => {
   if (event.target.id === 'jokeButton') {
-    newJoke()
+    newJoke();
+    gifSearch();
     console.log("done");
 
   }
